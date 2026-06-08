0f505e96b8d258631fb2a7dcbe044ec2f153d263
jarwo-debug <jarwoshankz@gmail.com>
2026-06-09 01:16:48 +0700

    upload ulang

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Select from "react-select";
 
 function App() {
+  const [datas, setDatas] = useState([]);
+  const [userSelect, setUserSelect] = useState("");
+  const [isShow, setIsShow] = useState(false);
+
+  const getBerries = async () => {
+    const berries = await fetch("https://pokeapi.co/api/v2/berry/");
+    const value = await berries.json();
+    let result = value.results.map((data) => {
+      return {
+        label: data.name,
+        value: data.name,
+      };
+    });
+    setDatas(result.sort((a, b) => a.label.localeCompare(b.label)));
+  };
+
+  useEffect(() => {
+    getBerries();
+  }, []);
+
+  const handleSubmit = () => {
+    setIsShow((state) => !state);
+  };
+
+  const handleChange = (value) => {
+    setUserSelect(value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{isShow ? userSelect : " "}</h1>
+      <button onClick={() => handleSubmit()} disabled={!userSelect}>
+        {isShow ? "Hide Values" : "Show Values"}
+      </button>
+      <br />
+      <br />
+      <Select options={datas} onChange={(e) => handleChange(e.value)}></Select>
     </div>
   );
 }
